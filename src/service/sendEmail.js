cd48d50dbef3e5b2fb6f0dc1160067fae3694457
auto-commit <auto@local>
2025-08-25 20:44:00 +0300

    first commit

--- src/service/sendEmail.js
+++ src/service/sendEmail.js
@@ -1,26 +1,29 @@
-import nodemailer from 'nodemailer';
+import nodemailer from "nodemailer";
 
-export const sendEmail = async ({to,subject,html,attachments = []})=>{
-    const transporter = nodemailer.createTransport({
-        port: 465,
-        secure:true,
-        service: 'gmail',
-        auth: {
-            user: process.env.EMAIL_USER,
-            pass: process.env.EMAIL_PASSWORD
-        }
-    });
-    const info = await transporter.sendMail({
-        from: process.env.EMAIL_USER,
-        to:to || " ",
-        subject : subject||"Hello from Saraha App",
-        text: "This is a test email from Saraha App",
-        html: html|| "<b>This is a test email from Saraha App</b>",
-        attachments: attachments || [] 
-    })
-    if(info.accepted.length > 0){
-        return true
-    } else{
-        return false
-    }
-}
+export const sendEmail = async ({ to, subject, html, attachments = [] }) => {
+  const transporter = nodemailer.createTransport({
+    port: 465,
+    secure: true,
+    service: "gmail",
+    auth: {
+      user: process.env.EMAIL_USER,
+      pass: process.env.EMAIL_PASSWORD,
+    },
+    tls: {
+      rejectUnauthorized: false,
+    },
+  });
+  const info = await transporter.sendMail({
+    from: process.env.EMAIL_USER,
+    to: to || " ",
+    subject: subject || "Hello from Saraha App",
+    text: "This is a test email from Saraha App",
+    html: html || "<b>This is a test email from Saraha App</b>",
+    attachments: attachments || [],
+  });
+  if (info.accepted.length > 0) {
+    return true;
+  } else {
+    return false;
+  }
+};
